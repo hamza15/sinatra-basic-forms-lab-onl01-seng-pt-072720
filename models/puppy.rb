ea2cabd08242ee96fc3cb9ca0f5c366f9272cae7
Sardar Hamza Naqi <sardarhamzanaqi@gmail.com>
2020-10-23 05:03:56 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -4,7 +4,7 @@ class Puppy
   attr_accessor :name, :age 
   attr_reader :breed
   
-  def initialize(name, breed, age)
+  def initialize(name:, breed:, age:)
     @name = name
     @breed = breed
     @age = age
